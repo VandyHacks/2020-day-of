3bb53cdad80b4c7e4fb2ccfb9d32740003b53770
xuezzou <ilfam1999@gmail.com>
2020-09-28 10:04:07 -0500

    shift links to the left on mobile

--- src/components/importantLink.js
+++ src/components/importantLink.js
@@ -162,7 +162,7 @@ const ScheduleBox = () => {
 
 	if (isMobile) {
 		return (
-			<Container style={{ marginLeft: '0em' }}>
+			<Container style={{ marginLeft: '-0.7rem' }}>
 				<BoxTitleContainer style={{ width: '100%', margin: '0' }}>
 					<h4 style={{ fontSize: '1.7em', marginBottom: '1.7rem' }}>Important Links</h4>
 					<StyledULTopMoible>
